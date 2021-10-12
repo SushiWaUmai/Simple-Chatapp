b8f808b77536164ac7e40af91c4ea80b96263f34
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2021-10-12 09:12:26 +0200

    :zap: add tailwind and postcss

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello World");
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+app.use(express.json());
+
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+
+const port = process.env.PORT || 4000;
+
+io.on("connection", (socket) => {
+  console.log("client connected");
+
+  socket.on("message", (message) => {
+    console.log(message);
+    io.emit("message", message);
+  });
+});
+
+httpServer.listen(port, () =>
+  console.log(`Server is set up on http://localhost:${port}`)
+);
