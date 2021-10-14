e2fe8daaf2ac66bcf00ef40951a3ab49b910676d
SushiWaUmai <eugene.bb1@gmail.com>
2021-10-14 09:07:36 +0200

    feat: update navbar styles

--- packages/client/src/components/NavbarComponent.tsx
+++ packages/client/src/components/NavbarComponent.tsx
@@ -1,22 +1,30 @@
 import { FunctionComponent } from "react";
 import Link from "next/link";
+import { ChatAltIcon, InformationCircleIcon } from "@heroicons/react/outline";
 
 interface NavbarComponentProps {}
 
 const NavbarComponent: FunctionComponent<NavbarComponentProps> = () => {
   return (
-    <div className="p-3 bg-gray-300">
-      <div className="flex justify-between">
-        <span className="inline-block">
-          <Link href="/">
-            <h1 className="select-none transform hover:scale-105">Simple Chatapp</h1>
-          </Link>
-        </span>
-        <span className="inline-block">
-          <Link href="/about">
-            <h1 className="select-none transform hover:scale-105">About</h1>
-          </Link>
-        </span>
+    <div className="p-3 bg-gray-50 shadow-sm">
+      <div className="container lg:mx-auto">
+        <div className="flex justify-between">
+          <span className="inline-block">
+            <Link href="/">
+              <div className="btn flex align-middle items-center gap-x-3">
+                <ChatAltIcon className="w-10 h-10 inline" />
+                <h1 className="text-xl inline">Simple Chatapp</h1>
+              </div>
+            </Link>
+          </span>
+          <span className="flex align-middle items-center ">
+            <Link href="/about">
+              <div className="btn">
+                <h1 className="text-lg">About</h1>
+              </div>
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
   );
