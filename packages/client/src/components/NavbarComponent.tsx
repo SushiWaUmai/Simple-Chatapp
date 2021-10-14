957f696e686f536a2506f85402f577a1b4e4e8a9
SushiWaUmai <eugene.bb1@gmail.com>
2021-10-14 15:09:55 +0200

    feat: :lipstick: update navbar with shadow and btn effect

--- packages/client/src/components/NavbarComponent.tsx
+++ packages/client/src/components/NavbarComponent.tsx
@@ -6,21 +6,21 @@ interface NavbarComponentProps {}
 
 const NavbarComponent: FunctionComponent<NavbarComponentProps> = () => {
   return (
-    <div className="p-3 bg-gray-50 shadow-sm">
+    <div className="p-3 bg-gray-50 shadow-xl">
       <div className="container lg:mx-auto">
         <div className="flex justify-between">
           <span className="inline-block">
             <Link href="/">
-              <div className="btn flex align-middle items-center gap-x-3">
+              <div className="btn flex align-middle items-center gap-x-3 btn-effect">
                 <ChatAltIcon className="w-10 h-10 inline" />
-                <h1 className="text-xl inline">Simple Chatapp</h1>
+                <h1 className="text-xl inline font-bold">Simple Chatapp</h1>
               </div>
             </Link>
           </span>
-          <span className="flex align-middle items-center ">
+          <span className="flex align-middle items-center">
             <Link href="/about">
-              <div className="btn">
-                <h1 className="text-lg">About</h1>
+              <div className="btn btn-effect">
+                <h1 className="text-lg font-bold">About</h1>
               </div>
             </Link>
           </span>
